2de7034df5aaef7771ef33e34c371ccd35d8ff5b
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2024-02-06 18:51:28 +0100

    v1.0.2

--- HypeRulerHelper.js
+++ HypeRulerHelper.js
@@ -1,180 +1,182 @@
 /*!
-Hype RulerHelper 1.0.1
-Modified to include marker functionality with labels
-Copyright (c) 2024 Max Ziebell, (https://maxziebell.de). 
-MIT-license
-*/
+ * Hype RulerHelper 1.0.2
+ * Modified to include customizable marker colors and global marker indicators
+ * Copyright (c) 2024 Max Ziebell, (https://maxziebell.de). MIT-license
+ */
 
 /*
-* Version-History
-* 1.0.0  Initial release under MIT-license
-* 1.0.1  Added marker feature with labels.
-*/
+ * Version-History
+ * 1.0.0 Initial release under MIT-license
+ * 1.0.1 Added marker feature with labels
+ * 1.0.2 Added customizable marker colors and global marker indicators
+ */
 
 if ("HypeRulerHelper" in window === false) window['HypeRulerHelper'] = (function() {
-	let documentMarkers = [];
-	let sceneMarkers = [];
-	let resizeObserver = null;
+    let documentMarkers = [];
+    let sceneMarkers = [];
+    let resizeObserver = null;
 
-	function createHiPPICanvas(width, height) {
-		const ratio = window.devicePixelRatio;
-		const canvas = document.createElement("canvas");
-		canvas.width = width * ratio;
-		canvas.height = height * ratio;
-		canvas.style.width = width + "px";
-		canvas.style.height = height + "px";
-		canvas.style.zIndex = 9999;
-		canvas.style.pointerEvents = 'none';
-		canvas.style.backgroundColor = 'transparent';
-		canvas.getContext("2d").scale(ratio, ratio);
-		return canvas;
-	}
+    function createHiPPICanvas(width, height) {
+        const ratio = window.devicePixelRatio;
+        const canvas = document.createElement("canvas");
+        canvas.width = width * ratio;
+        canvas.height = height * ratio;
+        canvas.style.width = width + "px";
+        canvas.style.height = height + "px";
+        canvas.style.zIndex = 9999;
+        canvas.style.pointerEvents = 'none';
+        canvas.style.backgroundColor = 'transparent';
+        canvas.getContext("2d").scale(ratio, ratio);
+        return canvas;
+    }
 
-	function drawLabel(ctx, text, x, y, color) {
-		ctx.save();
-	
-		ctx.fillStyle = color;
-	
-		const padding = 2;
-		const metrics = ctx.measureText(text);
-		const textWidth = metrics.width;
-		const boxWidth = textWidth + padding * 4; 
-		const boxHeight = 10 + padding * 2;
-	
-		ctx.beginPath();
-		ctx.moveTo(x + 2, y - boxHeight / 2);
-		ctx.lineTo(x + boxWidth - 2, y - boxHeight / 2);
-		ctx.quadraticCurveTo(x + boxWidth, y - boxHeight / 2, x + boxWidth, y - boxHeight / 2 + 2);
-		ctx.lineTo(x + boxWidth, y + boxHeight / 2 - 2);
-		ctx.quadraticCurveTo(x + boxWidth, y + boxHeight / 2, x + boxWidth - 2, y + boxHeight / 2);
-		ctx.lineTo(x + 2, y + boxHeight / 2);
-		ctx.quadraticCurveTo(x, y + boxHeight / 2, x, y + boxHeight / 2 - 2);
-		ctx.lineTo(x, y - boxHeight / 2 + 2);
-		ctx.quadraticCurveTo(x, y - boxHeight / 2, x + 2, y - boxHeight / 2);
-		ctx.closePath();
-		ctx.fill();
-		
-		ctx.fillStyle = 'white';
-		ctx.fillText(text, x + padding * 2, y + 4);
-	
-		ctx.restore();
-	}
-	
-	function refreshCanvas(element) {
-		const existingCanvas = document.getElementById('HypeRulerHelper');
-		if (existingCanvas) {
-			existingCanvas.parentNode.removeChild(existingCanvas);
-		}
+    function drawLabel(ctx, text, x, y, color = 'grey') {
+        ctx.save();
 
-		const canvas = createHiPPICanvas(200, element.offsetHeight);
-		canvas.id = 'HypeRulerHelper';
-		document.body.appendChild(canvas);
-		
-		canvas.style.position = "absolute";
-		canvas.style.top = "0px";
-		canvas.style.left = "0px";
-		
-		const ctx = canvas.getContext('2d');
+        ctx.fillStyle = color;
 
-		function drawMarks() {
-			const totalHeight = canvas.height / window.devicePixelRatio;
-			const smallMarkLength = 10; 
-			const mediumMarkLength = 15;
-			const largeMarkLength = 20; 
-			const markStartX = 0; 
-			const padding = 2;
+        const padding = 2;
+        const metrics = ctx.measureText(text);
+        const textWidth = metrics.width;
+        const boxWidth = textWidth + padding * 4; 
+        const boxHeight = 10 + padding * 2;
 
-			for (let y = 0; y <= totalHeight; y += 10) {
-				let markLength;
-				if (y % 100 === 0) {
-					markLength = largeMarkLength;
-					ctx.fillText(`${y}px`, markStartX + largeMarkLength + padding, y + 3);
-				} else if (y % 50 === 0) {
-					markLength = mediumMarkLength;
-				} else {
-					markLength = smallMarkLength;
-				}
+        ctx.beginPath();
+        ctx.moveTo(x + 2, y - boxHeight / 2);
+        ctx.lineTo(x + boxWidth - 2, y - boxHeight / 2);
+        ctx.quadraticCurveTo(x + boxWidth, y - boxHeight / 2, x + boxWidth, y - boxHeight / 2 + 2);
+        ctx.lineTo(x + boxWidth, y + boxHeight / 2 - 2);
+        ctx.quadraticCurveTo(x + boxWidth, y + boxHeight / 2, x + boxWidth - 2, y + boxHeight / 2);
+        ctx.lineTo(x + 2, y + boxHeight / 2);
+        ctx.quadraticCurveTo(x, y + boxHeight / 2, x, y + boxHeight / 2 - 2);
+        ctx.lineTo(x, y - boxHeight / 2 + 2);
+        ctx.quadraticCurveTo(x, y - boxHeight / 2, x + 2, y - boxHeight / 2);
+        ctx.closePath();
+        ctx.fill();
 
-				ctx.beginPath();
-				ctx.moveTo(markStartX, y);
-				ctx.lineTo(markStartX + markLength, y);
-				ctx.stroke();
-			}
+        ctx.fillStyle = 'white';
+        ctx.fillText(text, x + padding * 2, y + 4);
 
-			ctx.strokeStyle = 'green';
-			documentMarkers.forEach(marker => {
-				const { position, label } = marker;
-				ctx.beginPath();
-				ctx.moveTo(0, position);
-				ctx.lineTo(60, position);
-				ctx.stroke();
+        ctx.restore();
+    }
 
-				if (label) {
-					drawLabel(ctx, label, 60, position, 'green');
-				}
-			});
+    function refreshCanvas(element) {
+        const existingCanvas = document.getElementById('HypeRulerHelper');
+        if (existingCanvas) {
+            existingCanvas.parentNode.removeChild(existingCanvas);
+        }
 
-			ctx.strokeStyle = 'orange';
-			sceneMarkers.forEach(marker => {
-				const { position, label } = marker;
-				ctx.beginPath();
-				ctx.moveTo(0, position);
-				ctx.lineTo(60, position);
-				ctx.stroke();
+        const canvas = createHiPPICanvas(200, element.offsetHeight);
+        canvas.id = 'HypeRulerHelper';
+        document.body.appendChild(canvas);
 
-				if (label) {
-					drawLabel(ctx, label, 60, position, 'orange');
-				}
-			});
-		}
+        canvas.style.position = "absolute";
+        canvas.style.top = "0px";
+        canvas.style.left = "0px";
 
-		function setupCanvas() {
-			ctx.fillStyle = 'grey';
-			ctx.strokeStyle = 'grey';
-			ctx.lineWidth = 1;
-			ctx.font = '10px Arial';
+        const ctx = canvas.getContext('2d');
 
-			drawMarks();
-		}
+        function drawMarks() {
+            const totalHeight = canvas.height / window.devicePixelRatio;
+            const smallMarkLength = 10; 
+            const mediumMarkLength = 15;
+            const largeMarkLength = 20; 
+            const markStartX = 0; 
+            const padding = 2;
 
-		setupCanvas();
-	}
+            for (let y = 0; y <= totalHeight; y += 10) {
+                let markLength;
+                if (y % 100 === 0) {
+                    markLength = largeMarkLength;
+                    ctx.fillText(`${y}px`, markStartX + largeMarkLength + padding, y + 3);
+                } else if (y % 50 === 0) {
+                    markLength = mediumMarkLength;
+                } else {
+                    markLength = smallMarkLength;
+                }
 
-	function HypeDocumentLoad(hypeDocument, element, event) {
-		hypeDocument.addMarker = function(position, label) {
-			documentMarkers.push({ position, label });
-			refreshCanvas(element);
-		};
+                ctx.beginPath();
+                ctx.moveTo(markStartX, y);
+                ctx.lineTo(markStartX + markLength, y);
+                ctx.stroke();
+            }
 
-		hypeDocument.addSceneMarker = function(position, label) {
-			sceneMarkers.push({ position, label });
-			refreshCanvas(element);
-		};
-	}
+            // Draw document markers
+            documentMarkers.forEach(marker => {
+                const { position, label, color = 'green' } = marker;
+                ctx.strokeStyle = color;
+                ctx.beginPath();
+                ctx.moveTo(0, position);
+                ctx.lineTo(60, position);
+                ctx.stroke();
 
-	function HypeScenePrepareForDisplay(hypeDocument, element, event) {
-		sceneMarkers = [];
-		
-		if (resizeObserver) {
-			resizeObserver.disconnect();
-		}
+                if (label) {
+                    drawLabel(ctx, "🌐 " + label, 60, position, color); // Prepend unicode world icon to label
+                }
+            });
 
-		refreshCanvas(element);
+            // Draw scene markers
+            sceneMarkers.forEach(marker => {
+                const { position, label, color = 'orange' } = marker;
+                ctx.strokeStyle = color;
+                ctx.beginPath();
+                ctx.moveTo(0, position);
+                ctx.lineTo(60, position);
+                ctx.stroke();
 
-		resizeObserver = new ResizeObserver(entries => {
-			refreshCanvas(element);
-		});
+                if (label) {
+                    drawLabel(ctx, label, 60, position, color);
+                }
+            });
+        }
 
-		resizeObserver.observe(element);
-	}
+        function setupCanvas() {
+            ctx.fillStyle = 'grey';
+            ctx.strokeStyle = 'grey';
+            ctx.lineWidth = 1;
+            ctx.font = '10px Arial';
 
-	if("HYPE_eventListeners" in window === false) { window.HYPE_eventListeners = Array(); }
-	window.HYPE_eventListeners.push({type: "HypeDocumentLoad", callback: HypeDocumentLoad});
-	window.HYPE_eventListeners.push({type: "HypeScenePrepareForDisplay", callback: HypeScenePrepareForDisplay});
+            drawMarks();
+        }
 
-	const HypeRulerHelper = {
-		version: '1.0.1',
-	};
+        setupCanvas();
+    }
 
-	return HypeRulerHelper;
+    function HypeDocumentLoad(hypeDocument, element, event) {
+        hypeDocument.addMarker = function(position, label, color) {
+            documentMarkers.push({ position, label, color });
+            refreshCanvas(element);
+        };
+
+        hypeDocument.addSceneMarker = function(position, label, color) {
+            sceneMarkers.push({ position, label, color });
+            refreshCanvas(element);
+        };
+    }
+
+    function HypeScenePrepareForDisplay(hypeDocument, element, event) {
+        sceneMarkers = [];
+
+        if (resizeObserver) {
+            resizeObserver.disconnect();
+        }
+
+        refreshCanvas(element);
+
+        resizeObserver = new ResizeObserver(entries => {
+            refreshCanvas(element);
+        });
+
+        resizeObserver.observe(element);
+    }
+
+    if("HYPE_eventListeners" in window === false) { window.HYPE_eventListeners = Array(); }
+    window.HYPE_eventListeners.push({type: "HypeDocumentLoad", callback: HypeDocumentLoad});
+    window.HYPE_eventListeners.push({type: "HypeScenePrepareForDisplay", callback: HypeScenePrepareForDisplay});
+
+    const HypeRulerHelper = {
+        version: '1.0.2',
+    };
+
+    return HypeRulerHelper;
 })();
